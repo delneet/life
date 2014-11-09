7f1738d1fd60e748f806af3035e76f56e5609a15
Daniel <danieldelnaaij@gmail.com>
2014-11-09 17:13:08 +0100

    Use full screen
    Use all the rows and colls available in the window

--- lib/game.rb
+++ lib/game.rb
@@ -12,21 +12,19 @@ Curses.curs_set(0)
 
 module Game
   class Game
-    attr_reader   :size
-    attr_reader   :squares
-    
-    def initialize(size)
-      @size  = size
-      @win = Curses::Window.new(0,0,0,0)
-      @grid = Matrix.build(@size, @size * 2){nil}
+  
+    def initialize
+      @rows = Curses.lines
+      @cols = Curses.cols
+      @win = Curses::Window.new(@rows, @cols, 0, 0)
+      @grid = Matrix.build(@rows, @cols){nil}
       seed
     end
     
     def seed    
-      size.times do |row|
-        (size*2).times do |col|
+      @rows.times do |row|
+        @cols.times do |col|
           s = Square.new(row, col, alive)
-          puts "#{s.row},#{s.col}"
           @grid.send :[]=, row, col, s
         end
       end        
@@ -47,8 +45,8 @@ module Game
     def inspect_neighbours
       @grid.each_with_index do |e, row, col|  
         alive_neighbours = 0
-        (e.row-1..e.row+1).to_a.reject{|i| i<0 || i>@size-1}.each do |grow|
-          (e.col-1..e.col+1).to_a.reject{|i| i<0 || i>@size*2-1}.each do |gcol|
+        (e.row-1..e.row+1).to_a.reject{|i| i<0 || i>@rows-1}.each do |grow|
+          (e.col-1..e.col+1).to_a.reject{|i| i<0 || i>@cols-1}.each do |gcol|
             next if (e.row == grow && e.col == gcol)
             alive_neighbours += 1 if @grid[grow, gcol].alive?
           end
@@ -68,6 +66,6 @@ module Game
   end
 end
 
-game = Game::Game.new(30)
+game = Game::Game.new
 game.run
 
