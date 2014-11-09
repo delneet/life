e056551bc9320b1fe184a62f7b1ded61a0cb13e1
Daniel <danieldelnaaij@gmail.com>
2014-11-09 17:05:47 +0100

    replace empty cells with whitespaces
    Removing the empty cell characters and replacing them with whitespaces looks cleaner

--- lib/game.rb
+++ lib/game.rb
@@ -18,13 +18,13 @@ module Game
     def initialize(size)
       @size  = size
       @win = Curses::Window.new(0,0,0,0)
-      @grid = Matrix.build(@size){nil}
+      @grid = Matrix.build(@size, @size * 2){nil}
       seed
     end
     
     def seed    
       size.times do |row|
-        size.times do |col|
+        (size*2).times do |col|
           s = Square.new(row, col, alive)
           puts "#{s.row},#{s.col}"
           @grid.send :[]=, row, col, s
@@ -40,7 +40,7 @@ module Game
       while true do
         inspect_neighbours
         draw
-        #sleep(1)
+        sleep(0.1)
       end
     end
     
@@ -48,7 +48,7 @@ module Game
       @grid.each_with_index do |e, row, col|  
         alive_neighbours = 0
         (e.row-1..e.row+1).to_a.reject{|i| i<0 || i>@size-1}.each do |grow|
-          (e.col-1..e.col+1).to_a.reject{|i| i<0 || i>@size-1}.each do |gcol|
+          (e.col-1..e.col+1).to_a.reject{|i| i<0 || i>@size*2-1}.each do |gcol|
             next if (e.row == grow && e.col == gcol)
             alive_neighbours += 1 if @grid[grow, gcol].alive?
           end
